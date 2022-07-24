8e8a5645b461ac3d1d025bb0bfcf00041ccfff02
Azim <azim040407@yandex.ru>
2022-07-24 18:58:33 +0400

    cho-to podelal no takoe sebe

---
+++ Assets/scripts/menu/MainMenuButtons.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void Exit() => Application.Quit();
+    public void StartGame()
+    {
+        SceneManager.LoadScene("alloto");
+    }
+}
---
+++ Assets/scripts/player/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed { get; }
+    public float Hp { get; }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
