68cf04a316a62878d33f82fe74f46a22cc67fedd
Azim <azim040407@yandex.ru>
2023-01-26 06:42:48 +0400

    state and hp indicators are working

--- Assets/scripts/gameSceneUI/GameInfoController.cs
+++ Assets/scripts/gameSceneUI/GameInfoController.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameInfoController : MonoBehaviour
 {
+    private Image state, hp;
     void Start()
     {
         transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform);
@@ -11,20 +14,44 @@ public class GameInfoController : MonoBehaviour
         var rectTransform = GetComponent<RectTransform>();
         rectTransform.anchoredPosition = new Vector3(0f, -145f, 1f);
         rectTransform.sizeDelta = new Vector2(0f, -290f);               //ля я хз поч эти значения, но ток так робит
+        SetImageLists();
         SetPlayerData();
     }
 
     void Update()
     {
-        
+
     }
 
     private void SetPlayerData()
     {
+        hp = transform.GetChild(0).GetComponent<Image>();
+        state = transform.GetChild(1).GetComponent<Image>();
         Debugger.Log(CurrentProgress.currentProgress.Player.IsRage.ToString());
         CurrentProgress.currentProgress.Player.onChange += () =>
         {
             Debugger.Log("message from stans. IsRage = " + CurrentProgress.currentProgress.Player.IsRage);
+            UpdateStats();
         };
+
+        UpdateStats();
     }
+
+    private void UpdateStats()
+    {
+        state.sprite = states[CurrentProgress.currentProgress.Player.IsRage ? 1 : 0];
+        var maxHp = CurrentProgress.currentProgress.Player.MaxHealth;
+        var curHp = CurrentProgress.currentProgress.Player.CurrentHealth;
+        hp.sprite = hps[(int)Math.Ceiling(curHp * 5 / maxHp) - 1];
+        Debugger.Log(((int)Math.Ceiling(curHp * 5 / maxHp) - 1).ToString());
+    }
+
+    private void SetImageLists()
+    {
+        hps = Resources.LoadAll<Sprite>("uiImages/gameSceneUI/hp").Reverse().ToArray();
+        states = Resources.LoadAll<Sprite>("uiImages/gameSceneUI/states");
+    }
+
+    private Sprite[] hps;
+    private Sprite[] states;
 }
