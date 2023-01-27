f338807ab6e32cae728c10c554fbacc4ba41b1ba
Azim <azim040407@yandex.ru>
2023-01-27 20:25:20 +0400

    fun phrases by click

--- Assets/scripts/gameSceneUI/GameInfoController.cs
+++ Assets/scripts/gameSceneUI/GameInfoController.cs
@@ -6,8 +6,9 @@ using UnityEngine.UI;
 
 public class GameInfoController : MonoBehaviour
 {
-    private Image state, hp;
-    private Text funPhrase, npcName, npcInfo;
+    private GameInfoView view;
+    private Sprite[] hps;
+    private Sprite[] states;
     void Start()
     {
         transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform);
@@ -16,7 +17,7 @@ public class GameInfoController : MonoBehaviour
         rectTransform.anchoredPosition = new Vector3(0f, -145f, 1f);
         rectTransform.sizeDelta = new Vector2(0f, -290f);               //ля я хз поч эти значения, но ток так робит
         SetImageLists();
-        SetFields();
+        view = new GameInfoView(transform);
         SetPlayerData();
     }
 
@@ -37,22 +38,13 @@ public class GameInfoController : MonoBehaviour
         UpdateStats();
     }
 
-    private void SetFields()
-    {
-        hp = transform.GetChild(0).GetComponent<Image>();
-        state = transform.GetChild(1).GetComponent<Image>();
-        funPhrase = transform.GetChild(7).GetComponent<Text>();
-        npcName = transform.GetChild(8).GetComponent<Text>();
-        npcInfo = transform.GetChild(9).GetComponent<Text>();
-    }
-
     private void UpdateStats()
     {
-        state.sprite = states[CurrentProgress.currentProgress.Player.IsRage ? 1 : 0];
+        var st = states[CurrentProgress.currentProgress.Player.IsRage ? 1 : 0];
         var maxHp = CurrentProgress.currentProgress.Player.MaxHealth;
         var curHp = CurrentProgress.currentProgress.Player.CurrentHealth;
-        hp.sprite = hps[(int)Math.Ceiling(curHp * 5 / maxHp) - 1];
-        Debugger.Log(((int)Math.Ceiling(curHp * 5 / maxHp) - 1).ToString());
+        var hp = hps[(int)Math.Ceiling(curHp * 5 / maxHp) - 1];
+        view.UpdateStats(hp, st);
     }
 
     private void SetImageLists()
@@ -60,7 +52,4 @@ public class GameInfoController : MonoBehaviour
         hps = Resources.LoadAll<Sprite>("uiImages/gameSceneUI/hp").Reverse().ToArray();
         states = Resources.LoadAll<Sprite>("uiImages/gameSceneUI/states");
     }
-
-    private Sprite[] hps;
-    private Sprite[] states;
 }
---
+++ Assets/scripts/gameSceneUI/GameInfoView.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+public class GameInfoView
+{
+    private Image state, hp;
+    private Text funPhrase, npcName, npcInfo;
+    private Button location, map, menu, diary, inventory, funPhraseButton;
+
+    public GameInfoView(Transform transform)
+    {
+        hp          = transform.GetChild(0).GetComponent<Image>();
+        state       = transform.GetChild(1).GetComponent<Image>();
+
+        funPhrase   = transform.GetChild(7).GetComponent<Text>();
+        npcName     = transform.GetChild(8).GetComponent<Text>();
+        npcInfo     = transform.GetChild(9).GetComponent<Text>();
+
+        location    = transform.GetChild(2).GetComponent<Button>();
+        diary       = transform.GetChild(3).GetComponent<Button>();
+        map         = transform.GetChild(4).GetComponent<Button>();
+        inventory   = transform.GetChild(5).GetComponent<Button>();
+        menu        = transform.GetChild(6).GetComponent<Button>();
+        funPhraseButton = transform.GetChild(7).GetComponent<Button>();
+        SetButtonsOnClick();
+    }
+
+    private void SetButtonsOnClick()
+    {
+        funPhraseButton.onClick.AddListener(UpdatePhrase);
+    }
+
+    private void UpdatePhrase()
+    {
+        funPhrase.text = FunPhrases.GetRandom();
+    }
+
+    public void UpdateStats(Sprite hp, Sprite state)
+    {
+        this.hp.sprite = hp;
+        this.state.sprite = state;
+    }
+}
+
