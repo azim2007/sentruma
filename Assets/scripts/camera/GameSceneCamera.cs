c5f136c2e4062e461c8403fd5c1f3ada719ba364
Azim <azim040407@yandex.ru>
2023-12-06 09:02:24 +0400

    made road

--- Assets/scripts/camera/GameSceneCamera.cs
+++ Assets/scripts/camera/GameSceneCamera.cs
@@ -1,8 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections.Generic;
 
 public class GameSceneCamera : MonoBehaviour
 {
     private GameObject player;
+    private List<Tuple<float, float, float, float>> allotoBordersTopBotLefRig = 
+        new List<Tuple<float, float, float, float>>
+    {
+        Tuple.Create(7f,-4f,-1f,3f),
+    };
+    public Vector3 CameraPosition()
+    {
+        var borders = allotoBordersTopBotLefRig;
+        if(SceneManager.GetActiveScene().name == "alloto_main")
+            borders = allotoBordersTopBotLefRig;
+
+        //не сработает нужно на отдельных сценах все делать
+        return new Vector3();
+    }
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -10,7 +27,6 @@ public class GameSceneCamera : MonoBehaviour
 
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, 
-            player.transform.position.y, -10);
+        transform.position = CameraPosition();
     }
 }
